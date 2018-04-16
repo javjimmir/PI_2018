800a1aeb5c80bd99d98514ecaee9e4af96b650c3
FranAlcon96 <franplayer96@gmail.com>
2018-04-16 11:33:12 +0000

    Update and rename login.php to login_usuario.php

--- PI_2018/php/login_usuario.php
+++ PI_2018/php/login_usuario.php
@@ -9,7 +9,7 @@ session_start();
 $host = "localhost";
 $usuario = "root";
 $password = "root";
-$database = "PI_2018";
+$database = "actividades";
 
 $conexion = new mysqli($host, $usuario, $password, $database);
 
@@ -17,15 +17,10 @@ if ($conexion->connect_error) {
  die("La conexion falló: " . $conexion->connect_error);
 }
 
-$username = $_POST['username']; //usuario o empresa, dependiendo del tipo de login que se vaya a seleccionar.
+$username = $_POST['username']; 
 $password = $_POST['password']; 
-$tipo_login = $_POST['tipo_login'];
-
-// la problemática surge a la hora de manipular el select, hay que mirar como se hace. 
  
 $sql_usuarios = "SELECT nombre FROM usuario WHERE alias = '$username' AND password = '$password'"; // por ahora solo nos quedamos con el nombre para probar
-
-$sql_empresas = "SELECT cif FROM empresa WHERE nombre = '$username' AND password = '$password'"; // nos quedamos con el cif
 
 // desconexión de la base de datos.
 mysqli_close($conexion); 
