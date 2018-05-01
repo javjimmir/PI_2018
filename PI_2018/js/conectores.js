fd92a7b914dcdc45596a101cee9a183c11d70ade
cristianciclo <cristiandelossantosciclo@gmail.com>
2018-05-01 21:49:03 +0200

    Add files via upload

---
+++ PI_2018/js/conectores.js
@@ -0,0 +1,6 @@
+ $(document).ready(function () {
+      $('.menuLogin').load('./components/menuLogin/menuLogin.html');
+      $('.menuPrincipalUser').load('./components/menuPrincipalUser/menuPrincipalUser.html');
+      $('.pie').load('./components/pie/pie.html');
+      $('.filtroBusqueda').load('./components/filtroBusqueda/filtroBusqueda.html');
+    });
