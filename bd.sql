17202796ca3232a52c49495058ad636732fdf18b
cristianciclo <cristiandelossantosciclo@gmail.com>
2018-05-06 20:44:11 +0200

    Add files via upload

--- bd.sql
+++ bd.sql
@@ -6,6 +6,7 @@ CREATE TABLE usuario(
 	nif varchar(30) NOT NULL,
 	nombre varchar(25),
 	apellidos varchar(30),
+	telefono varchar(9),
 	pais varchar(25),
 	alias varchar(25),
 	email varchar(50),
@@ -20,6 +21,10 @@ CREATE TABLE usuario(
 CREATE TABLE empresa(
 	cif varchar(30) NOT NULL,
 	nombre varchar(25),
+	telefono varchar(9),
+	provincia varchar(30),
+	municipio varchar(30),
+	alias varchar(30),
 	tipo_actividad varchar(30),
 	web varchar(50),
 	email varchar(50),
@@ -33,6 +38,11 @@ CREATE TABLE oferta(
 	id int NOT NULL,
 	cif_empresa varchar(9),
 	nombre varchar(25),
+	provincia varchar(30),
+	municipio varchar(30),
+	duracion varchar(30),
+	num_plazas int,
+	tipo_actividad varchar(30),
 	descripcion varchar(150),
 	localizacion varchar(30),
 	precio double(5,2),
@@ -46,14 +56,12 @@ CREATE TABLE oferta(
 CREATE TABLE reserva(
 	id int NOT NULL,
 	nif_usuario varchar(9),
-	cif_empresa varchar(9),
 	id_oferta int,
 	fecha_reserva date,
 	num_plazas_reserva int,
 	coste_reserva double(5,2),
 	PRIMARY KEY (id),
 	FOREIGN KEY (nif_usuario) REFERENCES usuario(nif),
-	FOREIGN KEY (cif_empresa) REFERENCES empresa(cif),
 	FOREIGN KEY (id_oferta) REFERENCES oferta(id)
 );
 -- INSERT EN USUARIO
@@ -82,11 +90,11 @@ insert into oferta values(2,'11111111h','snorkel','lorem ipsum','guadalquivir',2
 
 
 -- INSERT EN RESERVA
-insert into reserva values(1,'47342916S','11111111k',1,'18-04-14',3,61.5);
-insert into reserva values(2,'47342918S','11111111k',1,'18-04-14',4,82.0);
+insert into reserva values(1,'47342916S',1,'18-04-14',3,61.5);
+insert into reserva values(2,'47342918S',1,'18-04-14',4,82.0);
 
 -- insert erroneo
-insert into reserva values(1,'47342916S','11111111k',1,'18-04-14',3,61.5);
-insert into reserva values(3,'47342920S','11111111k',1,'18-04-14',4,82.0);
-insert into reserva values(4,'47342916S','11111112k',1,'18-04-14',4,82.0);
-insert into reserva values(3,'47342916S','11111111k',2,'18-04-14',4,82.0);
+insert into reserva values(1,'47342916S',1,'18-04-14',3,61.5);
+insert into reserva values(3,'47342920S',1,'18-04-14',4,82.0);
+insert into reserva values(4,'47342916S',1,'18-04-14',4,82.0);
+insert into reserva values(3,'47342916S',2,'18-04-14',4,82.0);
