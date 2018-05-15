4f8cc9333e4192e836df90ab1b1f88de46f08c38
juandels3 <juandelsal3@gmail.com>
2018-05-15 17:11:48 +0200

    Agregadas categorías y quitados los insert erroneos de la bd

--- PI_2018/index.php
+++ PI_2018/index.php
@@ -9,8 +9,26 @@
 session_start();
 include 'php/connection.php';
 
-$global_sql = 'SELECT * FROM oferta ORDER BY fecha_inicio DESC'; // Esta será la sql estandar, que carga las ofertas, y puede ser modificada por los filtros!!
-$global_cont_sql = "SELECT COUNT(*) AS `count` FROM `oferta`";
+$global_sql = 'SELECT * FROM oferta ORDER BY fecha_inicio DESC'; // Sql global que carga las ofertas; por defecto seleccionará todas las ofertas más recientes
+$global_cont_sql = "SELECT COUNT(*) AS `count` FROM `oferta`";   // Contador de ofertas; por defecto contará todas las ofertas de la bd
+
+
+/**
+ *
+ *      Comprobación de categoría
+ *
+ */
+if (isset($_GET['category'])) {
+    $categoria = $_GET['category'];
+    $global_cont_sql = "SELECT COUNT(*) AS `count` FROM `oferta` WHERE categoria = "."'$categoria'";
+    $global_sql = "select * from oferta where categoria = "."'$categoria'";
+}
+
+/**
+ *
+ *      Comprobación de filtros
+ *
+ */
 
 // Se comprueba si se ha recibido un parámetro post mediante algún filtro
 if (isset($_POST["tipo_actividad"])) {
@@ -129,7 +147,7 @@ if (isset($_POST["tipo_actividad"])) {
 			$ofertas_encontradas = $fila['count'];
 
 			if ($ofertas_encontradas == 0) {
-                echo '<div id="sin_ofertas"><p>NO HAY OFERTAS EN LA BD PARA EL FILTRO '.$actividad.'</p></div>';
+                echo '<div id="sin_ofertas"><p>NO HAY OFERTAS DISPONIBLES</p></div>';
             }
 
 
