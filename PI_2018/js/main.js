b6724c95ebb78da7910e4c7b9072498d47f1dd39
juandels3 <juandelsal3@gmail.com>
2018-05-16 18:03:19 +0200

    added ajax superfilters

--- PI_2018/js/main.js
+++ PI_2018/js/main.js
@@ -6,24 +6,74 @@ function changeRangeValue(val){
     document.getElementById("range").value = isNaN(parseInt(val, 10)) ? 0 : parseInt(val, 10);
 }
 $(document).ready(function () {
-    // Intento de petición ajax
-    $("#range").change(function() {
-        var precio = $("#number").val();
-        var count = 0;
-        $.post("php/filter.php", {precio: precio, count: count}, function (data) {
+    $(".tipoact").change(function() {
+        var tipo_actividad = $('input[name=tipo_actividad]:checked', '#myform').val()
+        console.log(tipo_actividad)
+
+        /* Petición ajax que envía el tipo de actividad marcado */
+
+        $.post("php/filter.php", {tipo_actividad: tipo_actividad}, function (data) { // Le pasamos el tipo de actividad, que es lo que se procesará en servidor
             $(".tabla").empty();
             data = $.parseJSON(data);
             for (var i = 0; i <= data.length-1; i++) {
                 //console.log(data[i]);
                 $(".tabla").append("<div class='col-lg-4 actividad'>" +
                     "<div class='row'>" + "<div class='col-lg-4'>" + "<img src='./img/submarinismo.jpg' alt='submarinismo' class='listImg'></div>" +
-                "<div class='col-lg-8'>" +
-                "<p id='nombre'>" + data[i].nombre + "</p>" +
+                    "<div class='col-lg-8'>" +
+                    "<p id='nombre'>" + data[i].nombre + "</p>" +
                     "<p id='actividad'>" + data[i].provincia + "</p>" +
                     "<p id='provincia'>" + data[i].tipo_actividad + "</p>" +
                     "<p id='dificultad'>" + data[i].dificultad + "</p>" +
-                    "<p id='precio'>" + data[i].precio + "</p></div></div>");
+                    "<p id='precio'>" + data[i].precio + "€</p></div></div>");
             }
         });
+    })
+
+
+        // Petición ajax que se lanza cuando se desliza la barra de precio
+    $("#range").change(function() {
+        if ($('.tipoact').is(':checked')) {     // Si tipo de actividad está marcada, se enviará con el post precio y actividad
+            var precio = $("#number").val();
+            var tipo_actividad = $('input[name=tipo_actividad]:checked', '#myform').val()
+            console.log(tipo_actividad + " --- " + precio);
+
+            /* Petición ajax que envía el precio seleccionado en el rango */
+
+            $.post("php/filter.php", {precio: precio, tipo_actividad: tipo_actividad}, function (data) { // Le pasamos el precio, que es lo que se procesará en servidor
+                $(".tabla").empty();
+                data = $.parseJSON(data);
+                for (var i = 0; i <= data.length - 1; i++) {
+                    //console.log(data[i]);
+                    $(".tabla").append("<div class='col-lg-4 actividad'>" +
+                        "<div class='row'>" + "<div class='col-lg-4'>" + "<img src='./img/submarinismo.jpg' alt='submarinismo' class='listImg'></div>" +
+                        "<div class='col-lg-8'>" +
+                        "<p id='nombre'>" + data[i].nombre + "</p>" +
+                        "<p id='actividad'>" + data[i].provincia + "</p>" +
+                        "<p id='provincia'>" + data[i].tipo_actividad + "</p>" +
+                        "<p id='dificultad'>" + data[i].dificultad + "</p>" +
+                        "<p id='precio'>" + data[i].precio + "€</p></div></div>");
+                }
+            });
+        } else {    // Si no hay tipo de actividad marcada, hará el post solo con el precio
+            var precio = $("#number").val();
+
+            /* Petición ajax que envía el precio seleccionado en el rango */
+
+            $.post("php/filter.php", {precio: precio}, function (data) { // Le pasamos el precio, que es lo que se procesará en servidor
+                $(".tabla").empty();
+                data = $.parseJSON(data);
+                for (var i = 0; i <= data.length - 1; i++) {
+                    //console.log(data[i]);
+                    $(".tabla").append("<div class='col-lg-4 actividad'>" +
+                        "<div class='row'>" + "<div class='col-lg-4'>" + "<img src='./img/submarinismo.jpg' alt='submarinismo' class='listImg'></div>" +
+                        "<div class='col-lg-8'>" +
+                        "<p id='nombre'>" + data[i].nombre + "</p>" +
+                        "<p id='actividad'>" + data[i].provincia + "</p>" +
+                        "<p id='provincia'>" + data[i].tipo_actividad + "</p>" +
+                        "<p id='dificultad'>" + data[i].dificultad + "</p>" +
+                        "<p id='precio'>" + data[i].precio + "€</p></div></div>");
+                }
+            });
+        }
     })
 })
