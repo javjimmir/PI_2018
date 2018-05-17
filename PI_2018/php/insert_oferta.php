a32c2ef04ce5f1e9426dc58e5947c119bf99083e
FranAlcon96 <franplayer96@gmail.com>
2018-05-17 12:25:20 +0200

    Add files via upload

---
+++ PI_2018/php/insert_oferta.php
@@ -0,0 +1,37 @@
+<?php
+	include 'connection.php';
+	session_start();
+
+	/* Extracción del cif de la empresa mediante el alias proporcionado por la variable de sesión, no se controla que esta variable sea null o no puesto que no se puede llegar aquí si no estás logueado como una empresa. */
+
+	$sql_cif = "SELECT cif from empresa where alias='".$_SESSION['nombre']."'";
+
+	$result = $conexion->query($sql_cif); 
+	$fila = $row = $result->fetch_assoc();
+
+	$cif = $fila['cif'];
+	$nombre = $_POST['nombre'];
+	$provincia = $_POST['provincia-ofe'];
+	$municipio = $_POST['municipio'];
+	$duracion = $_POST['duracion'];
+	$plazas = $_POST['plazas'];
+	$actividad = $_POST['actividad'];
+	$localizacion = $_POST['localizacion'];
+	$precio = $_POST['precio'];
+	$dificultad = $_POST['dificultad'];
+	$categoria = $_POST['categoria'];
+	$img = "ruta de la imagen";
+	$fecha_inicio = $_POST['inicio'];
+	$fecha_fin = $_POST['fin'];
+	  
+	$sql = "INSERT INTO oferta (cif_empresa,nombre,provincia,municipio,duracion,num_plazas,tipo_actividad,localizacion,precio,dificultad,categoria,imagen_perfil,categoria,fecha_inicio,fecha_fin)
+	VALUES ('$cif','$nombre','$provincia','$municipio','$duracion',$plazas,'$actividad','$localizacion',$precio,'$dificultad','$categoria','$img','$fecha_inicio','$fecha_fin')";
+
+	if ($conexion->query($sql) === TRUE) {
+	    echo "Registro añadido correctamente.";
+	    header('location: ./success.html');
+	} else {
+	    echo "Error: " . $sql . "<br>" . $conexion->error;
+	}
+
+?>	
