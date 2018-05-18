04b5222200b87617fdf56f373cb2ddd5bcb40468
javjimmir <javierjiemenezmiranda@gmail.com>
2018-05-18 13:21:54 +0200

    vistas de registro usu empre componente publicidad

--- PI_2018/js/conectores.js
+++ PI_2018/js/conectores.js
@@ -3,4 +3,5 @@
       $('.menuPrincipalUser').load('./components/menuPrincipalUser/menuPrincipalUser.html');
       $('.pie').load('./components/pie/pie.html');
       $('.filtroBusqueda').load('./components/filtroBusqueda/filtroBusqueda.html');
+      $('.publicidad').load('./components/publicidad/publicidad.html')
     });
