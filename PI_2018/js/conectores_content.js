053def7bc2a32ef3f9195ec85ace8b5eee9206b4
javjimmir <javierjiemenezmiranda@gmail.com>
2018-05-21 14:48:29 +0200

    conection_content.js y vistas de registro

---
+++ PI_2018/js/conectores_content.js
@@ -0,0 +1,7 @@
+ $(document).ready(function () {
+      $('.menuLogin').load('../components/menuLogin/menuLogin.html');
+      $('.menuPrincipalUser').load('../components/menuPrincipalUser/menuPrincipalUser.html');
+      $('.pie').load('../components/pie/pie.html');
+      $('.filtroBusqueda').load('../components/filtroBusqueda/filtroBusqueda.html');
+      $('.publicidad').load('../components/publicidad/publicidad.html')
+    });
