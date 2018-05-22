964a20ee95a75331e38b4d9ab1197f2f5a518277
javjimmir <javierjiemenezmiranda@gmail.com>
2018-05-22 11:00:56 +0200

    vista reservas

--- PI_2018/content/reservas.php
+++ PI_2018/content/reservas.php
@@ -13,7 +13,7 @@ include '../php/connection.php';
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  	<script type="text/javascript" src="../js/conectores.js"></script>
+  	<script type="text/javascript" src="../js/conectores_content.js"></script>
 	<script type="text/javascript" src="../js/main.js"></script>
 	<title>Mis reservas</title>
 </head>
@@ -55,7 +55,7 @@ include '../php/connection.php';
 <nav class="menuPrincipalUser">
 
 </nav>
-<aside class="filtroBusqueda">
+<aside class="publicidad">
 
 </aside>
 
