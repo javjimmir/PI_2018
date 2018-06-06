179efa126d1aa17e748e99af92951e961b8db50c
javjimmir <javierjiemenezmiranda@gmail.com>
2018-06-06 12:48:04 +0200

    revision de errores formularios

--- PI_2018/js/validacion_reg_ofertas.js
+++ PI_2018/js/validacion_reg_ofertas.js
@@ -6,9 +6,12 @@ $(function () {
         if (name == "") {
             $("#error-oferta").html("<p> Complete el campo Nombre</p>");
             $("#error-oferta").fadeIn();
-            $("#nombre-ofe").focus();
+            $("#nombre-ofe").css('color', 'white');
+            $("#nombre-ofe").css('background', '#ff8080');
         }else{
             $("#error-oferta").fadeOut();
+            $("#nombre-ofe").css('background', '#e6ffcc');
+            $("#nombre-ofe").css('color', 'black');
         }
     });
 
@@ -18,9 +21,12 @@ $(function () {
         if (duraRegex.test(duracion) == false || duracion == "") {
             $("#error-oferta").html("<p>La duración de la actividad debe estar comprendida entre 1 y 999 min</p>");
             $("#error-oferta").fadeIn();
-            $("#duracion").focus();
+            $("#duracion").css('color', 'white');
+            $("#duracion").css('background', '#ff8080');
         }else{
             $("#error-oferta").fadeOut();
+            $("#duracion").css('background', '#e6ffcc');
+            $("#duracion").css('color', 'black');
         }
     });
 
@@ -30,9 +36,12 @@ $(function () {
         if (plazaRegex.test(plaza) == false || plaza == "") {
             $("#error-oferta").html("<p>Numero de plazas entre 1 y 99</p>");
             $("#error-oferta").fadeIn();
-            $("#plazas").focus();
+            $("#plazas").css('color', 'white');
+            $("#plazas").css('background', '#ff8080');
         }else{
             $("#error-oferta").fadeOut();
+            $("#plazas").css('background', '#e6ffcc');
+            $("#plazas").css('color', 'black');
         }
     });
 
@@ -42,9 +51,12 @@ $(function () {
         if (precioRegex.test(precio) == false || precio == "") {
             $("#error-oferta").html("<p>La precio de la actividad debe estar comprendida entre 1 y 600 €</p>");
             $("#error-oferta").fadeIn();
-            $("#precio").focus();
+            $("#precio").css('color', 'white');
+            $("#precio").css('background', '#ff8080');
         }else{
             $("#error-oferta").fadeOut();
+            $("#precio").css('background', '#e6ffcc');
+            $("#precio").css('color', 'black');
         }
     });
 
@@ -54,9 +66,12 @@ $(function () {
         if (name == "") {
             $("#error-oferta").html("<p> Complete el campo Actividad</p>");
             $("#error-oferta").fadeIn();
-            $("#actividad").focus();
+            $("#actividad").css('color', 'white');
+            $("#actividad").css('background', '#ff8080');
         }else{
             $("#error-oferta").fadeOut();
+            $("#actividad").css('background', '#e6ffcc');
+            $("#actividad").css('color', 'black');
         }
     });
 
@@ -66,9 +81,12 @@ $(function () {
         if (name == "") {
             $("#error-oferta").html("<p> Complete el campo Municipio</p>");
             $("#error-oferta").fadeIn();
-            $("#municipio-ofe").focus();
+            $("#municipio-ofe").css('color', 'white');
+            $("#municipio-ofe").css('background', '#ff8080');
         }else{
             $("#error-oferta").fadeOut();
+            $("#municipio-ofe").css('background', '#e6ffcc');
+            $("#municipio-ofe").css('color', 'black');
         }
     });
 
@@ -77,9 +95,12 @@ $(function () {
     if (date == "") {
         $("#error-oferta").html("<p> Complete el Fecha de inicio</p>");
         $("#error-oferta").fadeIn();
-        $("#date-ini").focus();
+        $("#date-ini").css('color', 'white');
+        $("#date-ini").css('background', '#ff8080');
     }else{
         $("#error-oferta").fadeOut();
+        $("#date-ini").css('background', '#e6ffcc');
+        $("#date-ini").css('color', 'black');
     }
     });
 
@@ -88,9 +109,12 @@ $(function () {
         if (date == "") {
             $("#error-oferta").html("<p> Complete el Fecha de finalización</p>");
             $("#error-oferta").fadeIn();
-            $("#date-fin").focus();
+            $("#date-fin").css('color', 'white');
+            $("#date-fin").css('background', '#ff8080');
         }else{
             $("#error-oferta").fadeOut();
+            $("#date-fin").css('background', '#e6ffcc');
+            $("#date-fin").css('color', 'black');
         }
 
         var date = new Date($('#date-ini').val());
@@ -98,9 +122,12 @@ $(function () {
         if (date >= date1){
             $("#error-oferta").html("<p>la fecha de finalización no puede ser anterior a la de inicio</p>");
             $("#error-oferta").fadeIn();
-            $("#date-fin").focus();
+            $("#date-fin").css('color', 'white');
+            $("#date-fin").css('background', '#ff8080');
         }else{
             $("#error-oferta").fadeOut();
+            $("#date-fin").css('background', '#e6ffcc');
+            $("#date-fin").css('color', 'black');
         }
     });
 
