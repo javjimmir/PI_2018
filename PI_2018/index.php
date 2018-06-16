9f7d478dd6ea97fe3308404773bb94db6fac7152
cristianciclo <cristiandelossantosciclo@gmail.com>
2018-06-16 11:26:58 +0200

    Añadido componente menuLogin en index

--- PI_2018/index.php
+++ PI_2018/index.php
@@ -34,43 +34,8 @@ if (isset($_GET['category'])) {
 </head>
 <body>
 <!-- ESTO ES LO MISMO QUE EL COMPONENTE, BASTA CON VOLVER A SUSTITUIRLO, CON LA DIFERENCIA DE QUE EL COMPONENTE NO ENLAZA BIEN CON EL LOGIN -->
-<header class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand nombre-web" href="#">WildSports</a>
-    </div>
-    <div id="logo"><img src="img/LogoAplicacion.png"/></div>
-    <ul class="nav navbar-nav navbar-right">
-    	<?php
-        /**
-         *
-         *      Submenú lateral de usuario/empresa conectado
-         *
-         */
-        // Si el usuario NO está logueado.
-        if (!isset($_SESSION['nombre'])) {
-            echo '<li><a href="content/selec_reg.php"><span class="glyphicon glyphicon-download-alt"></span> Registrarse</a></li>';
-            echo '<li><a href="content/login.html"><span class="glyphicon glyphicon-log-in"></span> Entrar</a></li>';
-            // Si el usuario está logueado, y visualizará distintos menús dependiendo de si es EMPRESA o USUARIO.
-        } else {
-            echo '
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="glyphicon glyphicon-user"></span> ' . $_SESSION['nombre']  . '
-                    <span class="caret"></span></a>
-                    <ul class="dropdown-menu">';
-            if ($_SESSION['tipo'] === "usuario") {
-                echo '<li><a href="content/perfil.php"><span class="glyphicon glyphicon-cog"></span> Mi perfil</a></li>
-                          <li><a href="content/reservas.php"><span class="glyphicon glyphicon-th-list"></span> Mis reservas</a></li>';
-            } else if ($_SESSION['tipo'] === "empresa") {
-                echo '<li><a href="content/perfil.php"><span class="glyphicon glyphicon-briefcase"></span> Perfil de empresa</a></li>';
-            }
-            echo '<li><a href="php/logout.php"><span class="glyphicon glyphicon-log-out"></span> Cerrar sesión</a></a></li>
-                    </ul>
-                </li>';
-        }
-        ?>
-    </ul>
-  </div>
+<header class="menuLogin">
+  
 </header>
 <nav class="menuPrincipalUser">
 
