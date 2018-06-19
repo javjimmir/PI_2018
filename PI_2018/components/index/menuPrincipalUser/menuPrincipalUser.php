1a03c5d4f4116ef5f74e2bf5d54cb21d5e6c109e
FranAlcon96 <franplayer96@gmail.com>
2018-06-19 13:00:39 +0200

    corregida opción de acuáticas

--- PI_2018/components/index/menuPrincipalUser/menuPrincipalUser.php
+++ PI_2018/components/index/menuPrincipalUser/menuPrincipalUser.php
@@ -5,30 +5,32 @@ session_start();
 <!DOCTYPE html>
 <html>
 <head>
-  <link rel="stylesheet" type="text/css" href="./css/style.css">
-  <link rel='stylesheet' id='BNS-Corner-Logo-Style-css'  href='./css/social_icons_from_Techandallcom.css' type='text/css' media='screen' />
+
+  <link rel="stylesheet" type="text/css" href="../css/style.css">
+  <link rel='stylesheet' id='BNS-Corner-Logo-Style-css'  href='../css/social_icons_from_Techandallcom.css' type='text/css' media='screen' />
+
 </head>
 <body>
   <div class="navbar navbar-default">
   <div class="container-fluid">
     <ul class="nav navbar-nav">
-      <li><a href="index.php">Inicio</a></li>
+      <li><a href="../index.php">Inicio</a></li>
       <li class="dropdown">
         <a class="dropdown-toggle" data-toggle="dropdown" href="#">Categorías
         <span class="caret"></span></a>
         <ul class="dropdown-menu">
-          <li><a href="index.php?category=acuatica">Acuáticas</a></li>
-          <li><a href="index.php?category=tierra">Tierra</a></li>
-          <li><a href="index.php?category=aire">Aire</a></li>
-          <li><a href="index.php?category=nieve">Nieve</a></li>
+          <li><a href="../index.php?category=agua">Acuáticas</a></li>
+          <li><a href="../index.php?category=tierra">Tierra</a></li>
+          <li><a href="../index.php?category=aire">Aire</a></li>
+          <li><a href="../index.php?category=nieve">Nieve</a></li>
         </ul>
       </li>
-      <li><a href="./content/comofunciona.php">Cómo Funciona</a></li>
-      <li><a href="./content/contacto.php">Contacto</a></li>
-      <li><a href="./content/acercade.php">Acerca de</a></li>
+      <li><a href="comofunciona.php">Como Funciona</a></li>
+      <li><a href="contacto.php">Contacto</a></li>
+      <li><a href="acercade.php">Acerca de</a></li>
         <?php
         if (!empty($_SESSION) && $_SESSION['tipo'] === "empresa") {
-            echo "<li><a href='content/activity_manager.php' style=\"color:#2196f3\">Administrar actividades</a></li>";
+            echo "<li><a href='activity_manager.php' style=\"color:#2196f3\">Administrar actividades</a></li>";
         }
         ?>
     </ul>
