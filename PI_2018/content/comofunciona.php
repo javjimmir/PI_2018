50185c33059e71439756013cac3b523b0062ac22
Freddiew20 <deckerexomg@gmail.com>
2018-06-20 22:50:34 +0200

    Añadido fondo dinámico de nuevo

--- PI_2018/content/comofunciona.php
+++ PI_2018/content/comofunciona.php
@@ -14,6 +14,7 @@ include '../php/connection.php';
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="../js/fondo.js"></script>
     <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <script type="text/javascript" src="../js/conectores_content.js"></script>
     <script type="text/javascript" src="../js/validacion_reg_empre.js"></script>
--- PI_2018/js/main.js
+++ PI_2018/js/main.js
@@ -58,6 +58,23 @@ $(document).ready(function () {
         });
     });
 
+                /*Fondo dinámico */
+                var d = new Date();
+                var n = d.getMonth();
+            
+                if(n>10 && n<2){
+                    // invierno
+                            $(document.body).css('background-image', 'url(http://www.hdbilder.eu/p/get_photo/544286/2560/1440)');
+                }else if(n>=2 && n<5){
+                    // primavera
+                            $(document.body).css('background-image', 'url(http://www.thetravelboss.com/images/gallery/img_1515493479.jpg)');
+                }else if(n>=5 && n<8){
+                    // verano
+                            $(document.body).css('background-image', 'url(https://www.sunsportsmaine.com/wp-content/uploads/2017/12/GoPro-Hero-Session-Floaty-3.jpg)');
+                }else if(n>=8 && n<11){
+                    // otoño 
+                            $(document.body).css('background-image', 'url(https://hdwallsource.com/img/2014/9/downhill-wallpaper-hd-35545-36355-hd-wallpapers.jpg)');
+                }
 
     if (getUrlParameter('status') == 'success') {
         $(".status").append("<span class='alert alert-success'>¡Imagen actualizada con éxito!</span>").delay(3000).fadeOut();
@@ -210,3 +227,4 @@ $(document).ready(function () {
         }
     });
 });
+
